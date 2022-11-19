7f24557883165c9044c94ca9f9a8f084a850d666
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-19 06:17:46 +0100

    slight visual adjustments, button logic fixed

--- main.js
+++ main.js
@@ -11,14 +11,22 @@ const originalTitle = document.querySelector('.original-title')
 const originalTitleRomanised = document.querySelector('.original-title-romanised')
 const bgImage = document.querySelector('.apiDisplay')
 const results = document.querySelector('.results')
+        const titleArray = []
+        const originalTitleArray = []
+        const originalTitleRomanisedArray = []
+        const dateArray = []
+        const directorArray = []
+        const runtimeArray = []
+        const rtScoreArray = []
+        const imageArray = []
 
 
 button.addEventListener('click', apiRequest)
 
 //Syncing the enter key to trigger a click event
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
+input.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+      e.preventDefault();
       button.click();
     }
   });
@@ -29,28 +37,20 @@ async function apiRequest(){
         const response = await fetch(`https://ghibliapi.herokuapp.com/films`)
         const data = await response.json()
         console.log(data)
-        const titleArray = []
-        const originalTitleArray = []
-        const originalTitleRomanisedArray = []
-        const dateArray = []
-        const directorArray = []
-        const runtimeArray = []
-        const rtScoreArray = []
-        const imageArray = []
         
         for(let i = 0; i < data.length; i++){
-            titleArray.push(data[i].title)
-            originalTitleArray.push(data[i].original_title)
-            originalTitleRomanisedArray.push(data[i].original_title_romanised)
-            dateArray.push(data[i].release_date)
+        titleArray.push(data[i].title)
+        originalTitleArray.push(data[i].original_title)
+        originalTitleRomanisedArray.push(data[i].original_title_romanised)
+        dateArray.push(data[i].release_date)
             directorArray.push(data[i].director)
             runtimeArray.push(data[i].running_time)
             imageArray.push(data[i].image)
             rtScoreArray.push(data[i].rt_score)
         }
-    
+            //fill the apiDisplay div content with the inputed movie title
             for(let j = 0; j < titleArray.length; j++){
-        if(titleName == data[j].title.toLowerCase() || titleName == data[j].title.toUpperCase()) {
+        if(titleName ==  data[j].title.toLowerCase() || titleName == data[j].title.toUpperCase()|| titleName == data[j].title) {
             console.log('works')
             console.log(titleArray)
             title.innerText = 'Movie Title: ' + titleArray[j]
@@ -61,6 +61,8 @@ async function apiRequest(){
             runtime.innerText = 'Movie runtime: ' + runtimeArray[j]
             rating.innerText = 'Movie RT rating: ' + rtScoreArray[j]
             image.src = imageArray[j]
+            
+            //show apiDisplay content, remove placeholder image
             bgImage.style.background = 'none'
                 image.style.visibility = 'visible'
                 results.style.visibility = 'visible'
